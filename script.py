b702e8233c9049933761e3338a0c85d8945a6fe4
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2022-11-04 22:42:13 +0200

    Add logo to images

--- script.py
+++ script.py
@@ -15,9 +15,9 @@ places = names_file['المركز'].tolist()
 
 certificatesImages = [
     './png-certificates/pass.png',
-    './png-certificates/first-place.png',
-    './png-certificates/second-place.png',
-    './png-certificates/third-place.png',
+    './png-certificates/1.png',
+    './png-certificates/2.png',
+    './png-certificates/3.png',
 ]
 
 
@@ -27,7 +27,7 @@ RED = '#cf403a'
 FONT = './fonts/Changa.ttf'
 
 
-def print_pass_certificate(name):
+def print_pass_certificate(name, index):
     img = Image.open(certificatesImages[0])
     image_editable = ImageDraw.Draw(img)
     text = reshaper.reshape(name)
@@ -35,7 +35,7 @@ def print_pass_certificate(name):
     font = ImageFont.truetype(FONT, 60)
     poss = (250, 300)
     image_editable.text(poss, text_to_display, font=font, fill=RED)
-    img.save("./output/pass/" + name + ".png")
+    img.save("./output/pass/" + str(index) + name + ".png")
 
 
 def print_place_certificate(place, name):
@@ -44,14 +44,13 @@ def print_place_certificate(place, name):
     text = reshaper.reshape(name)
     text_to_display = get_display(text)
     font = ImageFont.truetype(FONT, 110)
-    poss = (500, 1200)
-    image_editable.text(poss, text_to_display, font=font, fill=GOLD, direction='rtl')
+    poss = (500, 1070)
+    image_editable.text(poss, text_to_display, font=font, fill=GOLD)
     img.save("./output/" + str(place) + "/" + name + ".png")
 
 
 for index, name in enumerate(names):
-    if index == 0:
-        if places[index] > 0 and places[index] < 4:
-            print_place_certificate(places[index], name)
-
-        print_pass_certificate(name)
+    print_pass_certificate(name, index)
+    place = places[index]
+    if place <= 3:
+        print_place_certificate(place, name)
