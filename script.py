6d249252c6de107b5c51b2fa485c43814d0700cd
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2024-08-02 22:58:08 +0300

    Add new design & centering names

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from PIL import Image, ImageFont, ImageDraw
 from arabic_reshaper import ArabicReshaper
+from bidi.algorithm import get_display
 
 # Configuration for reshaping Arabic text
 configuration = {
@@ -17,13 +18,14 @@ places = names_file['المركز'].tolist()
 # Paths to certificate images
 certificatesImages = [
     './png-certificates/pass.png',
-    './png-certificates/1.png',
-    './png-certificates/2.png',
-    './png-certificates/3.png',
+    './png-certificates/1.jpeg',
+    './png-certificates/2.jpeg',
+    './png-certificates/3.jpeg',
 ]
 
 # Colors and font settings
-GOLD = '#ab802c'
+# GOLD = '#ab802c'
+GOLD = '#000'
 RED = '#cf403a'
 FONT = './fonts/Changa.ttf'
 
@@ -42,8 +44,13 @@ def print_place_certificate(place, name):
     image_editable = ImageDraw.Draw(img)
     # Reshape and apply bidi algorithm for correct Arabic display
     text = reshaper.reshape(name)
-    font = ImageFont.truetype(FONT, 110)
-    poss = (500, 1070)
+    text_to_display = get_display(text)
+    font = ImageFont.truetype(FONT, 50)
+    # Calculate the bounding box of the text
+    bbox = image_editable.textbbox((0, 0), text_to_display, font=font)
+    text_width = bbox[2] - bbox[0]
+    # Calculate x-coordinate to center the text
+    poss = ((img.width - text_width) / 2, 670)
     image_editable.text(poss, text, font=font, fill=GOLD)
     img.save("./output/" + str(place) + "/" + name + ".png")
 
@@ -51,6 +58,7 @@ def print_place_certificate(place, name):
 for index, name in enumerate(names):
     place = places[index]
     if place > 3:
-        print_pass_certificate(name, index)
+        print('ok')
+        # print_pass_certificate(name, index)
     else:
         print_place_certificate(place, name)
